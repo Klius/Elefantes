a054f938d8dcb944dd6a373dd3889c387e44cefb
Klius <Klius@users.noreply.github.com>
2017-06-29 14:45:30 +0200

    Update elefantes.js

--- elefantes.js
+++ elefantes.js
@@ -3,10 +3,10 @@ elefantes()
 function elefantes(){
 switch(numeroDeElefantes){
 	case 1:
-  	  		responsiveVoice.speak(numeroDeElefantes+" elefante se balanceaba sobre la tela de una araña y como veía que no se caía fueron a llamar a otro elefante","Spanish Female");
+  	  		responsiveVoice.speak(numeroDeElefantes+" elefante se balanceaba sobre la tela de una araña. y como veía que no se caía fueron a llamar a otro elefante","Spanish Female");
           break;
   default:
-  		responsiveVoice.speak(numeroDeElefantes+" elefantes se balanceaban sobre la tela de una araña y como veían que no se caían fueron a llamar a otro elefante","Spanish Female");
+  		responsiveVoice.speak(numeroDeElefantes+" elefantes se balanceaban sobre la tela de una araña. y como veían que no se caían fueron a llamar a otro elefante","Spanish Female");
   break;
 }
 
